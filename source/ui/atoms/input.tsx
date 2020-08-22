6d148f3a764afdbb605f802524c112909e6a31e3
Podivilov Evgeniy <evgeniy.podivilov@gmail.com>
2020-08-22 18:16:46 +0300

    feat: :construction: add available to set field value

--- source/ui/atoms/input.tsx
+++ source/ui/atoms/input.tsx
@@ -1,13 +1,17 @@
 import React, { FC } from 'react';
 
-type InputProps = Field;
+type InputProps = Omit<Field, 'label'>;
 
 export const Input: FC<InputProps> = ({
   id, type, name, value,
 }) => {
   if (type === 'textarea') {
-    return <textarea id={id} name={name} value={value} />;
+    return <textarea id={id} name={name} defaultValue={value} />;
   }
 
-  return <input id={id} type={type} name={name} value={value} />;
+  if (type === 'checkbox' || type === 'radio') {
+    return <input id={id} type={type} name={name} defaultChecked={Boolean(value)} />;
+  }
+
+  return <input id={id} type={type} name={name} defaultValue={value} />;
 };
--- source/ui/molecules/field.tsx
+++ source/ui/molecules/field.tsx
@@ -2,14 +2,12 @@ import React, { FC } from 'react';
 import { Label } from '../atoms/label';
 import { Input } from '../atoms/input';
 
-type FieldProps = {
-  id: string;
-  label: string;
-  type: FieldType;
-};
-export const Field: FC<FieldProps> = ({ id, type, label }) => (
+type FieldProps = Field;
+export const Field: FC<FieldProps> = ({
+  id, type, label, name, value,
+}) => (
   <>
     <Label htmlFor={id}>{label}</Label>
-    <Input id={id} type={type} />
+    <Input id={id} type={type} name={name} value={value} />
   </>
 );
